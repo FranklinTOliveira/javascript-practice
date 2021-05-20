289d66e77ee63e19013ce8a14ffac7982b5270f3
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-05-19 21:08:58 -0700

    I have established a html css and js file.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+
+function my_name() {
+    console.log("My name is Franklin Oliveira.");
+}
+
+function i_like() {
+    console.log("I have many favorite foods.", '\n', "I like to eat salmon, broccoli and potatoes.", '\n', "I like to drink pepsi or beer.");
+}
+
+
+
+
+
+
+
+
+
