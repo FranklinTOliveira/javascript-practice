3300966dab02641b8b97854b0af4ebcf2d0f0a87
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-05-20 20:27:07 -0700

    I tell the reader about myself through an interactive webpage.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 // JavaScript source code
 
 function my_name() {
-    console.log("My name is Franklin Oliveira.");
+    console.log("My name is Franklin Oliveira.")
+    document.querySelector('p').textContent="My name is Franklin Oliveira."
 }
+
 
 function i_like() {
-    console.log("I have many favorite foods.", '\n', "I like to eat salmon, broccoli and potatoes.", '\n', "I like to drink pepsi or beer.");
+    console.log("I have many favorite foods.", '\n', "I like to eat salmon, broccoli and potatoes.", '\n',
+        "I like to drink pepsi or beer.")
+    document.querySelector('p').textContent = "I have many favorite foods. I like to eat salmon, broccoli and potatoes. I like to drink pepsi or beer."
 }
-
-
-
-
-
-
-
-
-
