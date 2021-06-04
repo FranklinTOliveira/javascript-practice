b8cafe47398dd81e3b2341ed8ae5fff906afda9e
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-06-03 19:11:24 -0700

    Completed assignment.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,43 @@ function i_like() {
         "I like to drink pepsi or beer.")
     document.querySelector('p').textContent = "I have many favorite foods. I like to eat salmon, broccoli and potatoes. I like to drink pepsi or beer."
 }
+
+//Daily Challenge
+//alert user is number is even or odd
+//number as argument
+
+let isOdd = function (n) {
+    if (n % 2 === 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+let n = 1;
+alert(isOdd(n));
+
+const input = "somepassword";
+
+function startsWithLetter(characterCode) {
+    const firstLetterCode = characterCode.charCodeAt(0);
+
+    if (firstLetterCode >= 97 && firstLetterCode <= 122 || firstLetterCode >= 65 & firstLetterCode <= 90) {
+        alert("First character is a letter");
+    } else {
+        alert("First character is not a letter");
+    }
+}
+
+startsWithLetter(input);
+
+function validatePassword(password) {
+    const length = password.length;
+
+    if (length >= 6 && length <= 20) {
+        alert("Meets length requirement.");
+    } else {
+        alert("Doesn't meet length requirement.");
+    }
+}
+
+validatePassword(input);
