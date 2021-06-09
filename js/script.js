91068a0675e38cdaa2b3796a03ace645a6011b2b
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-06-08 17:50:02 -0700

    This task has been completed!

--- js/script.js
+++ js/script.js
@@ -1,37 +1,12 @@
 // JavaScript source code
 
-function job(state) {
-    return new Promise(function (resolve, reject) {
-        if (state) {
-            resolve('success');
-        } else {
-            reject('error');
-        }
-    });
-}
+// api url
+fetch("https://cat-fact.herokuapp.com/facts")
+.then(response => response.json())
+		.then(cats => {
+			const randomNumberInRange = Math.random() * cats.length;
+			const randomIndex = Math.floor(randomNumberInRange);
+			const randomCat = cats[randomIndex];
 
-let promise = job(true);
-
-promise
-
-    .then(function (data) {
-        console.log(data);
-
-        return job(false);
-    })
-
-    .catch(function (error) {
-        console.log(error);
-
-        return 'Error caught';
-    })
-
-    .then(function (data) {
-        console.log(data);
-
-        return job(true);
-    })
-
-    .catch(function (error) {
-        console.log(error);
-    });
+			console.log(randomCat.text);
+		});
