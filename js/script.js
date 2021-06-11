97d94ff798d53d01e3ea9f8c52567e660f2c3d60
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-06-10 20:52:54 -0700

    Awesome!

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 // JavaScript source code
 
 // api url
-fetch("https://cat-fact.herokuapp.com/facts")
-.then(response => response.json())
-		.then(cats => {
-			const randomNumberInRange = Math.random() * cats.length;
-			const randomIndex = Math.floor(randomNumberInRange);
-			const randomCat = cats[randomIndex];
+/*const generateFactoid = fetch("https://cat-fact.herokuapp.com/facts")
+    .then(response => response.json())
+    .then(cats => {
+        const randomNumberInRange = Math.random() * cats.length;
+        const randomIndex = Math.floor(randomNumberInRange);
+        const randomCat = cats[randomIndex];
 
-			console.log(randomCat.text);
-		});
+        const factButton = document.getElementById("answer").innerHTML;
+        factButton.addEventListener("click", randomCat);
+    });-->
+*/
+
+let file = "https://cat-fact.herokuapp.com/facts"
+
+fetch(file)
+    .then(x => x.text())
+    .then(y => document.getElementById("answer").innerHTML = y);
