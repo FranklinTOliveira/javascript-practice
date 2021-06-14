29d368e357d517dca47b640f65c04294f6f3d06a
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-06-13 20:08:27 -0700

    Almost done?

--- day-7.1/js/script.js
+++ day-7.1/js/script.js
@@ -1,13 +1,15 @@
 // JavaScript source code
 
-function my_name() {
-    console.log("My name is Franklin Oliveira.")
-    document.querySelector('p').textContent = "My name is Franklin Oliveira."
-}
-
-
-function i_like() {
-    console.log("I have many favorite foods.", '\n', "I like to eat salmon, broccoli and potatoes.", '\n',
-        "I like to drink pepsi or beer.")
-    document.querySelector('p').textContent = "I have many favorite foods. I like to eat salmon, broccoli and potatoes. I like to drink pepsi or beer."
+function fizzbuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
 }
