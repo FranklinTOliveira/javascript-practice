c635db2378b40bdf4d415c56aa67f14036d9acc0
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-06-13 19:36:41 -0700

    Day 4

--- day-4/js/script.js
+++ day-4/js/script.js
@@ -11,3 +11,32 @@ function i_like() {
         "I like to drink pepsi or beer.")
     document.querySelector('p').textContent = "I have many favorite foods. I like to eat salmon, broccoli and potatoes. I like to drink pepsi or beer."
 }
+
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+
+let a = Number(prompt("Enter first number"));
+let b = Number(prompt("Enter second number"));
+
+// Convert the values to integers
+// Store the two integers as variables
+
+let x = parseInt(a);
+let y = parseInt(b);
+
+// Compare the two numbers
+// Alert the larger number
+
+alert(Math.max(x, y));
+
+// ------------ TAKE HOME CHALLENGE ------------
+// Add an edge case to your daily challenge code
+// Check if the user input anything other than a number
+// If so, prompt the user again to input a number
+
+if (x === Number.NaN) {
+    prompt("Enter an actual number.")
+} else if (y === Number.NaN) {
+    prompt("Enter an actual number.");
+}
+
