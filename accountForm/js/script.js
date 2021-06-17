72b2c2ccb708d7f3748fb2712f6e724487558278
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-06-17 14:46:31 -0700

    Here be the instructions for the last lessons.

--- accountForm/js/script.js
+++ accountForm/js/script.js
@@ -1,2 +1,27 @@
 // JavaScript source code
 
+/*
+    Create a new codepen
+    Build a form for users to create an account
+    You will need:
+        Username field*
+        Password field*
+        Repeat Password field*
+        "Create Account" button
+        *required fields
+    Disable the "create account" button until all fields are completed
+    When the user clicks "Create Account", check if the passwords match
+    If all checks pass, store the user's information
+    console.log the username and password
+    BONUS: Display an error if the passwords don't match
+*/
+
+
+
+
+
+
+
+
+
+
--- day-9/js/script.js
+++ day-9/js/script.js
@@ -1,2 +1,11 @@
 // JavaScript source code
 
+
+/*
+    Create a new codepen
+    Create a contact form with a submit button
+    Using what you learned about DOM manipulation, create a submit function that will:
+        Grab the values entered by the user
+        Hide the form
+        Display the information submitted by the user
+*/
